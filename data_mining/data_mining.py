a722bda9b06217827bdb49b1585512b837965582
Joao <joaoheron@gmail.com>
2020-09-26 01:23:30 -0300

    Created CRISP stage descriptions

--- data_mining/data_mining.py
+++ data_mining/data_mining.py
@@ -1,1 +1,21 @@
-"""Main module."""
+"""
+    Cross-industry standard process for data mining
+
+1) Entendimento do negócio 
+esta etapa consiste em definir os objetivos do projeto de Data Mining com um olhar para os problemas de negócio. 
+É importante salientar que o ciclo de desenvolvimento do projeto só tem efeito se for orientado para resolver um problema de negócio
+
+2) Entendimento dos Dados 
+esta etapa consiste desde a captura dos dados até a identificação de problemas relacionados à qualidade.
+Nesta fase é também onde se formam hipóteses em cima do que se aprendeu com os dados.
+
+3) Preparação dos Dados 
+Selecionar os dados
+Limpar os dados (dados nulos, ausentes, zerados, inválidos, etc..)
+Construir dados (criar novos atributos a partir de outros)
+Integrar dados: combinar múltiplas tabelas ou outras fontes
+Formatar dados: modificações sintáticas nos dados, sem alterar o seu significado
+
+
+"""
+
