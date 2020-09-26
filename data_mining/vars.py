167edacb70ab7bc246378c42d007d177d8b98cce
Joao <joaoheron@gmail.com>
2020-09-26 01:24:16 -0300

    Created project default variables

---
+++ data_mining/vars.py
@@ -0,0 +1,16 @@
+download = 'download/'
+adult_data = 'adult.data'
+adult_names = 'adult.names'
+adult_test = 'adult.test'
+chromedriver_path = 'res/chromedriver'
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/%s'
+
+download_data = download + adult_data
+download_names = download + adult_names
+download_test = download + adult_test
+
+url_download_data = url % adult_data
+url_download_names = url % adult_names
+url_download_test = url % adult_test
+
+TIMEOUT=30
