829aff76ae3d80bb9d0bfd6cb913e2cd66b2474c
Joao <joaoheron@gmail.com>
2020-09-26 01:25:23 -0300

    Added extractor call to command line interface

--- data_mining/cli.py
+++ data_mining/cli.py
@@ -1,16 +1,31 @@
 """Console script for data_mining."""
 import sys
+import os
 import click
+from data_mining import crawler
 
+DB_URI = os.environ.get('DB_URI', None)
+
+def verify_environment_variables():
+    if DB_URI is None:
+        raise Exception(MESSAGE_ENV_VAR_NOT_SET % ('DB_URI'))
 
 @click.command()
-def main(args=None):
-    """Console script for data_mining."""
-    click.echo("Replace this message by putting your code into "
-               "data_mining.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+# @click.option("--folder", "f", default="dataml_demo_app", help="DataML webapp folder name.")
+def extract():
+    """
+        Extract data from website
+    """
+    try:
+        crawler.extract_data()
+    except Exception as e:
+        raise e
 
+@click.group()
+def entry_point():
+    pass
+
+entry_point.add_command(extract)
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(entry_point())  # pragma: no cover
